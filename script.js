e614a2d67a9078581db89ab8ea4c70147ff4f3d2
Helena Torres-Rocha <helena.torres-rocha@ocsbstudent.ca>
2021-02-19 05:03:49 +0000

    i

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let userGuess = 0
 
 function randomizer () {
   // get number typed in by user and convert to an intiger
-  userGuess = document.getElementById('input-1')  
-  .value
+  userGuess = document.getElementById('input-1')
+    .value
   userGuess = parseInt(userGuess)
 
   // generate random number between 1 and 6
@@ -14,6 +14,6 @@ function randomizer () {
   randomNumber = parseInt(randomNumber)
   // compare user's guess with random number
   if (userGuess === randomNumber) {
-  document.getElementById('answer-1').innerHTML = 'You Win!'
-    }
+    document.getElementById('answer-1').innerHTML = 'You Win!'
+  }
 }
